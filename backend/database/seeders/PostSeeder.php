429bf6501174e92d875fc6aa570a54ff1235c94f
Leonardo Silva <33772895+leonardo534@users.noreply.github.com>
2025-10-06 17:49:08 -0300

    Update PostSeeder.php
    Criação de post

--- backend/database/seeders/PostSeeder.php
+++ backend/database/seeders/PostSeeder.php
@@ -16,29 +16,25 @@ class PostSeeder extends Seeder
             'author_id' => 1,
         ]);
         Post::create([
-            'title' => 'Meu titulo 1',
-            'content' => 'Meu conteudo 1',
+            'title' => 'Meu titulo 2',
+            'content' => 'Meu conteudo 2',
             'author_id' => 1,
         ]);
         Post::create([
-            'title' => 'Meu titulo 2',
-            'content' => 'Meu conteudo 2',
-            'author_id' => 2,
+            'title' => 'Meu titulo 3',
+            'content' => 'Meu conteudo 3',
+            'author_id' => 1,
         ]);
-        // Post::create([
-        //     'title' => 'Meu titulo 3',
-        //     'content' => 'Meu conteudo 3',
-        //     'author_id' => 2,
-        // ]);
-        // Post::create([
-        //     'title' => 'Meu titulo 4',
-        //     'content' => 'Meu conteudo 4',
-        //     'author_id' => 3,
-        // ]);
-        // Post::create([
-        //     'title' => 'Meu titulo 5',
-        //     'content' => 'Meu conteudo 5',
-        //     'author_id' => 3,
-        // ]);
+        Post::create([
+            'title' => 'Meu titulo 4',
+            'content' => 'Meu conteudo 4',
+            'author_id' => 1,
+        ]);
+        Post::create([
+            'title' => 'Meu titulo 5',
+            'content' => 'Meu conteudo 5',
+            'author_id' => 1,
+        ]);
+        
     }
 }
